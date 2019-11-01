ce752d5ed1715f293aae7922aa784bcd507160b1
tomexar <tommyexar@gmail.com>
2019-11-01 11:35:21 -0600

    found some coins

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -12,6 +12,21 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    hash_table_insert(ht, weights[0],0)
+    current_index = 1
+    while current_index < length:
+        current_weight = weights[current_index]
+        hash_table_insert(ht, current_weight, current_index)
+        limit_pair = limit - current_weight
+        has_pair = hash_table_retrieve(ht, limit_pair)
+        if has_pair:
+            if current_index == 1:
+                return (current_index, 0)
+            elif has_pair > current_index:
+                return (has_pair, current_index)
+            else:
+                return (current_index, has_pair)
+        current_index += 1
 
     return None
 
